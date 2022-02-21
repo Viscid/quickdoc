8afb2b3ad398aea549b3a485a789fd54ee6914b5
Mark 'Viscid' Gillis <viscid@gmail.com>
2022-02-20 22:36:39 -0500

    Deletes readme, adds some formatting to frontpage

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,11 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-5xl"> quickdoc.io </h1>
-      <p> Ephemeral document repositories. </p>
+    <div className="p-12 flex flex-col justify-center items-center content-center h-[50vh]">
+      <div>
+        <h1 className="text-5xl font-bold text-center"> quickdoc.io </h1>
+        <p className="text-3xl text-center"> Ephemeral document repositories. </p>
+      </div>
     </div>
   );
 };
